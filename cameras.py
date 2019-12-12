fa0ada56aacd7ed443579dca8e4b55952072d6a1
David-UX31A <dhen2714@uni.sydney.edu.au>
2019-12-13 09:40:52 +1100

    Cameras module.

---
+++ cameras.py
@@ -0,0 +1,32 @@
+import cv2
+from PyQt5.QtGui import QImage
+import numpy as np
+
+
+class Webcam:
+    def __init__(self, cid=0):
+        self.cid = cid
+        self.camera = cv2.VideoCapture(self.cid)
+
+        ret, frame = self.camera.read()
+
+        self.height = frame.shape[0]
+        self.width = frame.shape[1]
+        self.channels = frame.shape[2]
+        self.bitdepth = frame.nbytes/(self.height*self.width*self.channels)
+
+        self.nullimg = np.zeros((self.height, self.width, self.channels), dtype=np.uint8)
+        self.qformat = QImage.Format_RGB888
+        self.frame_count = 0
+
+        self.max_exposure = 10
+        self.min_exposure = 0
+
+    def get_frame(self):
+        ret, frame = self.camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.frame_count += 1
+        return frame, self.frame_count
+
+    def set_exposure(self):
+        pass
